7b07c36670cfa71b647ac1aef66e77d510443060
trabalho-pc\Andre <andrluiz.alvares.alvares84@gmail.com>
2017-02-05 00:45:27 -0200

    New properties:     useInterpolationWhileGrabbing: if true, while grabbing, the group will temporally have your useInterpolation property to false     color: color of the panel, not customizable yet
    More comments!
    Now the :setPosition of the grabbing considers the parent position
    Now is possible to rename the title of the panel, just right-click on red title to rename, right-click again (or click off the title) to disable this mode

--- Ink/modules/Ink_panel.lua
+++ Ink/modules/Ink_panel.lua
@@ -1,43 +1,61 @@
 local Panel = assert(love.filesystem.load("Ink/modules/Ink_module.lua"))()
 
 function Panel:start (values, inkLib, name)
-    self.isGrabbed = false
     self.isResizable = true
-    self.isOnResize = false
     self.isTittleEditable = false
+    self.useInterpolationWhileGrabbing = false
+    self.lockHover = false
+    self.color = {200, 20, 20, 255}
 
     Panel(values, inkLib, name)
 
-    self.lockHover = false
+    self.isOnResize = false
+    self.isGrabbed = false
     self.isActive = false
+    self.isRenamingTitle = false
 end
 
 function Panel:mousepressed (x, y, b)
-    -- grab control
     if self.inHover then
+        -- set active
         self.isActive = true
-        if y < self.pos.y + 20 and b == 1 and not self.isOnResize then -- if is not resizing and the mouse is over tittle
-            self.isGrabbed = true
-            -- move the panel to the mouse position (considerating pivot)
-            self:setPosition(
-                love.mouse.getX() - (self.size.x * self.pivot.x),
-                love.mouse.getY() - (self.size.y * self.pivot.y)
-            )
 
-            if self.group ~= nil then
-                for i=1,#self.group do
-                    self.group[i].posInterpolDisabled = true
+
+         -- if is not resizing and the mouse is over tittle
+        if y < self.pos.y + 20 and not self.isOnResize then
+            -- if left click, enable grabbing
+            if b == 1 then
+                -- enable grabbing and disable title editing
+                self.isGrabbed = true
+
+                -- move the panel to the mouse position (considerating pivot)
+                self:setPosition(
+                    love.mouse.getX() - (self.size.x * self.pivot.x) - self.inkLib.instances[self.parent].pos.x,
+                    love.mouse.getY() - (self.size.y * self.pivot.y) - self.inkLib.instances[self.parent].pos.y
+                )
+
+                -- if is not to use Ink interpolation when grabbing, disable the interpolation of the group
+                if self.group ~= nil and not self.useInterpolationWhileGrabbing then
+                    for i=1,#self.group do
+                        self.group[i].posInterpolDisabled = true
+                    end
+                else
+                    self.posInterpolDisabled = true
                 end
-            else
-                self.posInterpolDisabled = true
+            -- elseif right click, enable or disable renaming
+            elseif b == 2 and self.isActive then
+                self.isRenamingTitle = not self.isRenamingTitle
             end
-        end
-
-        if y > self.pos.y + self.size.y - 10 and x > self.pos.x + self.size.x - 10 and b == 1  and not self.isGrabbed then
+        -- else if is not grabbing and the mouse is over the rezize icon
+        elseif y > self.pos.y + self.size.y - 10 and x > self.pos.x + self.size.x - 10 and not self.isGrabbed then
             self.sizeInterpolDisabled = true
             self.isOnResize = true
+        -- elseif is mouse over the panel area
+        elseif y > self.pos.y + 20 and y < self.pos.y + self.size.y - 10 then
+            self.isRenamingTitle = false
         end
     else
+        self.isRenamingTitle = false
         self.isActive = false
     end
 end
@@ -46,13 +64,13 @@ function Panel:mousereleased (x, y, b)
     if self.isGrabbed then
         -- fix the panel position on release
         self:setPosition(
-            love.mouse.getX() + (self.size.x * self.pivot.x),
-            love.mouse.getY() + (self.size.y * self.pivot.y)
+            love.mouse.getX() + (self.size.x * self.pivot.x) - self.inkLib.instances[self.parent].pos.x,
+            love.mouse.getY() + (self.size.y * self.pivot.y) - self.inkLib.instances[self.parent].pos.y
         )
         self.isGrabbed = false
     end
     self.isOnResize = false
-    if self.group ~= nil then
+    if self.group ~= nil and not self.useInterpolationWhileGrabbing then
         for i=1,#self.group do
             self.group[i].posInterpolDisabled = false
         end
@@ -67,8 +85,8 @@ function Panel:update ()
     if self.isGrabbed then
         -- move the panel to the mouse position (considerating pivot)
         self:setPosition(
-            love.mouse.getX() - (self.size.x * self.pivot.x),
-            love.mouse.getY() - (self.size.y * self.pivot.y)
+            love.mouse.getX() - (self.size.x * self.pivot.x) - self.inkLib.instances[self.parent].pos.x,
+            love.mouse.getY() - (self.size.y * self.pivot.y) - self.inkLib.instances[self.parent].pos.y
         )
     end
 
@@ -82,8 +100,29 @@ function Panel:update ()
     end
 end
 
-function Panel:draw (dt)
+function Panel:textinput (text)
+    if self.isRenamingTitle then
+        self.text = self.text .. text
+    end
+end
 
+function Panel:keypressed (key)
+    if self.isRenamingTitle then
+        if key == "backspace" then
+            -- [backspace code by love2d.org/wiki/utf8]
+            -- get the byte offset to the last UTF-8 character in the string.
+            local byteoffset = utf8.offset(self.text, -1)
+
+            if byteoffset then
+                -- remove the last UTF-8 character.
+                -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
+                self.text = string.sub(self.text, 1, byteoffset - 1)
+            end
+        end
+    end
+end
+
+function Panel:draw (dt)
     -- window box
     love.graphics.setColor(100,100,100)
     love.graphics.rectangle("fill", self.pos.x, self.pos.y, self.size.x, self.size.y)
@@ -91,12 +130,20 @@ function Panel:draw (dt)
     love.graphics.setColor(255,255,255)
     love.graphics.rectangle("line", self.pos.x, self.pos.y + 21, self.size.x, self.size.y - 20)
 
-    -- title color
+    -- set the title color
     if not self.isGrabbed then
-        love.graphics.setColor(200, 20, 20)
+        if self.isRenamingTitle then
+            self.color = {240, 150, 20, 255}
+        elseif self.isActive then
+            self.color = {220, 20, 20, 255}
+        else
+            self.color = {200, 20, 20, 255}
+            love.graphics.setColor(200, 20, 20)
+        end
     else
-        love.graphics.setColor(180, 40, 40)
+        self.color = {180, 40, 60, 255}
     end
+    love.graphics.setColor(self.color)
 
     -- title box
     love.graphics.rectangle("fill", self.pos.x, self.pos.y, self.size.x, 20)
@@ -108,6 +155,8 @@ function Panel:draw (dt)
     love.graphics.setColor(250, 250, 250)
     love.graphics.print(self.text, self.pos.x + 10, self.pos.y)
 
+
+
     -- the rezize icon
     local iniColor = 0
     if not self.isOnResize then
@@ -116,6 +165,7 @@ function Panel:draw (dt)
         iniColor = 200
     end
 
+    -- draw the resize icon (its 4 boxes with 4 different colors)
     for i=1,4 do
         love.graphics.setColor(iniColor, iniColor, iniColor)
         love.graphics.rectangle("fill", self.pos.x + self.size.x - 10, self.pos.y + self.size.y - 10, 10 - (2 * (i-1)), 10 - (2 * (i-1)))
